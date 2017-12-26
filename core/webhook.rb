64fb3dfd8a225b8cdd1ce480f442080381de29fc
Rizkie Pratama <personal@rizkiepratama.net>
2017-12-26 10:56:54 +0700

    Add Method to capture webhook request

--- core/webhook.rb
+++ core/webhook.rb
@@ -1,4 +1,6 @@
 require 'sinatra/base'
+require 'json'
+require 'fileutils'
 
 module TBot
     class Webhook < Sinatra::Base
@@ -9,7 +11,14 @@ module TBot
 	end
 
 	post '/' do
-	    'Processing Incoming Request!'
+	    tmp_dir = File.join(File.dirname(File.dirname(__FILE__)), 'tmp')
+	    unless File.directory?(tmp_dir)
+		'No TMP, Making One....'
+	        FileUtils.mkdir_p(tmp_dir)
+	    end
+            values = JSON.parse(request.env["rack.input"].read)
+	    File.open(File.join(tmp_dir, 'webhook_input.log'), 'w') { |log| log.write(values) }
+	    'Done..'
 	end
 
 	def sendResponse()
