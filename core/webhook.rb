001d54eefd5e6ae385bb75c70c7cb13c7e3e39fb
Rizkie Pratama <personal@rizkiepratama.net>
2017-12-27 17:07:11 +0700

    Fix response method, add Debug Line

--- core/webhook.rb
+++ core/webhook.rb
@@ -14,17 +14,23 @@ module TBot
 	post '/' do
 	    webhook_request = JSON.parse(request.env["rack.input"].read)
 	    if webhook_request.has_key? 'message'
-	        sendResponse(webhook_request['message']['chat']['id'], webhook_request['message'].to_json)
+                sendResponse(webhook_request['message']['chat']['id'], JSON.pretty_generate(webhook_request))
 	    else
 	        "Invalid Request!"
 	    end
 	end
 
 	def sendResponse(target_chat, message)
+	    if target_chat.nil? || message.nil?
+	        puts "Empty Response Returned"
+		return nil
+	    end
 	    uri = URI.parse(Config.api_url + 'sendMessage')
 	    headers = {"Content-Type" => "application/json"}
             http = Net::HTTP.new(uri.host, uri.port)
-	    result = http.post(uri.path, message, headers)
+	    response_json = {"chat_id": target_chat, "text": '```' + message + '```', "parse_mode": "markdown"}.to_json
+	    puts response_json
+	    result = http.post(uri.path, response_json, headers)
 	end
     end
 end
